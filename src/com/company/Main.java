c4460cb5f47b1ccb92fa8215076b31b732fc3011
stepanka.blazkova <stepanka.blazkova@post.cz>
2022-09-23 09:24:11 +0200

    uprava prikazu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Vítejte v nabídce aplikací");
+        System.out.println("Vítejte v nabídce aplikací!");
         int vyber;
         int score = 0;
         do {
